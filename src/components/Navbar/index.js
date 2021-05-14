a7089ad9ea1513994ded89f81eced006ccdf0c9e
nativedev2021 <nativedev2021@gmail.com>
2021-05-14 22:53:30 +0400

    edited footer section

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,14 +5,7 @@ const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
-        <div>
-          <NavLink to="/">Gifts.ae</NavLink>
-          <p style={{ color: "#FFF" }}>this site was made by Hassan Jarko</p>
-          <p style={{ color: "#FFF" }}>www.hassan.jarko@yahoo.com</p>
-          <p style={{ color: "#FFF" }}>
-            &copy; Copyright 2021. All Rights Reserved
-          </p>
-        </div>
+        <NavLink to="/">Gifts.ae</NavLink>
         <NavIcon onClick={toggle}>
           <p>Menu</p>
           <Bars />
